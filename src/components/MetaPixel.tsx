b5cff8ca5ddc7bec0a62836e490d217ee1261eb8
Marconi Castro <marconi.castro.mc@gmail.com>
2025-08-19 16:37:40 -0300

    Atualização final corrigida2 pixel

--- src/components/MetaPixel.tsx
+++ src/components/MetaPixel.tsx
@@ -1,3 +1,4 @@
+
 'use client'
 
 import { useEffect } from 'react'
@@ -21,29 +22,38 @@ export default function MetaPixel() {
         t.src=v;s=b.getElementsByTagName(e)[0];
         s.parentNode.insertBefore(t,s)}(window, document,'script',
         'https://connect.facebook.net/en_US/fbevents.js');
-        fbq(\'init\', \'1403975024017865\');
+        fbq('init', '1403975024017865');
       `
       document.head.appendChild(script)
 
       // Carregar o script do TikTok Pixel
-      const tiktokScript = document.createElement('script')
-      tiktokScript.innerHTML = `
-        !function (w, d, t) {
-          w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","load","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","bind"];ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.load=\'D2HMSD3C77U9B02M0HMG\';ttq.page();
-          }(window, document, \'ttq\');
-      `
-      document.head.appendChild(tiktokScript)
+      const tiktokScript = document.createElement("script");
+      tiktokScript.src = "https://analytics.tiktok.com/i18n/pixel/events.js?sdk_version=2.0.0&pixel_code=D2HMSD3C77U9B02M0HMG";
+      tiktokScript.async = true;
+      tiktokScript.onload = () => {
+        window.ttq = window.ttq || [];
+        window.ttq.methods = ["page", "track", "identify", "instances", "load", "debug", "on", "off", "once", "ready", "alias", "group", "enableCookie", "disableCookie", "bind"];
+        window.ttq.setAndDefer = function(t, e) {
+          t[e] = function() {
+            t.push([e].concat(Array.prototype.slice.call(arguments, 0)));
+          };
+        };
+        for (var i = 0; i < window.ttq.methods.length; i++) {
+          window.ttq.setAndDefer(window.ttq, window.ttq.methods[i]);
+        }
+        window.ttq.load = "D2HMSD3C77U9B02M0HMG";
+        window.ttq.page();
+      };
+      document.head.appendChild(tiktokScript);
 
       // Adicionar o noscript do TikTok (opcional, mas boa prática)
-      const tiktokNoscript = document.createElement('noscript')
+      const tiktokNoscript = document.createElement("noscript");
       tiktokNoscript.innerHTML = `
         <img height="1" width="1" style="display:none"
         src="https://analytics.tiktok.com/i18n/pixel/events.js?sdk_version=2.0.0&pixel_code=D2HMSD3C77U9B02M0HMG"
         />
-      `
-      document.head.appendChild(tiktokNoscript)
-
-      // Adicionar o noscript
+      `;
+      document.head.appendChild(tiktokNoscript);
       const noscript = document.createElement('noscript')
       noscript.innerHTML = `
         <img height="1" width="1" style="display:none"
@@ -63,9 +73,6 @@ export default function MetaPixel() {
   useEffect(() => {
     if (typeof window !== 'undefined' && window.fbq) {
       window.fbq('track', 'PageView')
-    }
-    if (typeof window !== 'undefined' && window.ttq) {
-      window.ttq.page()
     }
   }, [pathname, searchParams])
 
@@ -93,3 +100,5 @@ declare global {
     ttq: any
   }
 }
+
+
