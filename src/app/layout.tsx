e9784b89970cc207dcab9170e15964df0894a858
marconicastro <149693258+marconicastro@users.noreply.github.com>
2025-08-29 16:04:00 -0300

    Update layout.tsx
    ok

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,9 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { ReactNode } from "react";
-import Script from "next/script";
-
+import { Toaster } from "@/components/ui/toaster";
+import MetaPixel from "@/components/MetaPixel";
+import { Suspense } from 'react';
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="pt">
@@ -16,17 +18,12 @@ export default function RootLayout({ children }: { children: ReactNode }) {
           async
           defer
         />
+        <Toaster />
         {children}
       </body>
     </html>
   );
-}import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { Toaster } from "@/components/ui/toaster";
-import MetaPixel from "@/components/MetaPixel";
-import { Suspense } from 'react';
-
+}
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
